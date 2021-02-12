a78abd58aca63171cd4bd90bdd0a4446ec22f532
Shihanur Rahman Chowdhury <hackedgdown@gmail.com>
2021-02-12 11:52:49 +0600

    1. Introducntion of ES6

---
+++ 1.Intro_ES6.js
@@ -0,0 +1,30 @@
+/**************************
+* Bismillahir Rahmanir Rahim
+* ES6 T-01
+* Intoduction of ECMAScript 2016(ES6) 
+* Author : Shihan Chowdhury
+* Thanks to - Twinkle Cats 
+**************************/
+
+// what we are using JS that is ECMAScript 2015 or ES5
+// ECMAScript 6 was the second major revision to JavaScript.
+// ES6 main features:
+// # Let & Const
+// # Template String
+// # Arrow Functions
+// # Enhance Object
+// # Destructing
+// # Default parameter
+// # Map, Set
+// # Spread Operator
+// # Iterators
+// # Generators
+// # Class
+// # Inheritance
+// # Modules
+// # Promises
+
+// Qustion: Why should we use ES6 instead of ES5?
+// Answer: You can't find support form any community like stack overflow besideds ES6 is a trend in job field
+
+// *** Working with ES6 you need a compiler that is Babel
