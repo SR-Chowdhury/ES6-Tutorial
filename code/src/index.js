5963874bed5877dfe1483238d4c1d15c003636fb
Shihanur Rahman Chowdhury <hackedgdown@gmail.com>
2021-02-12 12:14:22 +0600

    Install babel for ES6; here .babelrc,  index.html, package-lock.json, package.json, webpack.config.js, index.js has been created

---
+++ code/src/index.js
@@ -0,0 +1,14 @@
+const a = 10;
+const b = 15;
+
+const sum = (a,b) => a + b;
+
+console.log(sum(a,b));
+
+/*here const sum = (a,b) => a + b; works: 
+
+    var sum = function(a, b) {
+        return a + b;
+    }
+    sum(5,10);
+*/
